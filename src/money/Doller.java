a72e067ac2261c1d37433dda816ceee09f929e08
shigeru <hironoki3@gmail.com>
2020-09-24 00:32:46 +0900

    fix Doller's side effects.

--- src/money/Doller.java
+++ src/money/Doller.java
@@ -1,13 +1,15 @@
 package money;
 
-public class Doller {
+import java.util.function.DoubleBinaryOperator;
+
+class Doller {
 	int amount;
 
 	Doller(int amount) {
 		this.amount = amount;
 	}
 
-	void times(int multiplier) {
-		amount *= multiplier;
+	Doller times(int multiplier) {
+		return new Doller(amount * multiplier);
 	}
 }
--- test/money/MoneyTest.java
+++ test/money/MoneyTest.java
@@ -8,7 +8,9 @@ public class MoneyTest {
 	@Test
 	public void testMultiplication() {
 		Doller five = new Doller(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Doller product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 }
